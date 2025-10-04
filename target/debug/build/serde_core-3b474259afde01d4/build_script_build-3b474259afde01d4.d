a3ed814efe48fbce167e89b2e725301268c84839
DrTomLLC <DrTomLLC@gmail.com>
2025-10-04 15:53:48 -0400

    chore(autosync): 2025-10-04T15:53:48

---
+++ target/debug/build/serde_core-3b474259afde01d4/build_script_build-3b474259afde01d4.d
@@ -0,0 +1,5 @@
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\build\serde_core-3b474259afde01d4\build_script_build-3b474259afde01d4.d: C:\Users\DrTom\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
+
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\build\serde_core-3b474259afde01d4\build_script_build-3b474259afde01d4.exe: C:\Users\DrTom\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
+
+C:\Users\DrTom\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs:
