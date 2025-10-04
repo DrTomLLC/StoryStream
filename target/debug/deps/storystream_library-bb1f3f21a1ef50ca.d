fe4b0dfd05e1aed70f17ae83010e5b21659e2dde
DrTomLLC <DrTomLLC@gmail.com>
2025-10-04 16:19:24 -0400

    chore(autosync): 2025-10-04T16:19:23

---
+++ target/debug/deps/storystream_library-bb1f3f21a1ef50ca.d
@@ -0,0 +1,10 @@
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_library-bb1f3f21a1ef50ca.d: crates\library\src\lib.rs crates\library\src\error.rs crates\library\src\import.rs crates\library\src\manager.rs crates\library\src\metadata.rs crates\library\src\scanner.rs
+
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_library-bb1f3f21a1ef50ca.rmeta: crates\library\src\lib.rs crates\library\src\error.rs crates\library\src\import.rs crates\library\src\manager.rs crates\library\src\metadata.rs crates\library\src\scanner.rs
+
+crates\library\src\lib.rs:
+crates\library\src\error.rs:
+crates\library\src\import.rs:
+crates\library\src\manager.rs:
+crates\library\src\metadata.rs:
+crates\library\src\scanner.rs:
---
+++ target/debug/deps/storystream_library-f382248a20a8b30c.d
@@ -0,0 +1,10 @@
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_library-f382248a20a8b30c.d: crates\library\src\lib.rs crates\library\src\error.rs crates\library\src\import.rs crates\library\src\manager.rs crates\library\src\metadata.rs crates\library\src\scanner.rs
+
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_library-f382248a20a8b30c.rmeta: crates\library\src\lib.rs crates\library\src\error.rs crates\library\src\import.rs crates\library\src\manager.rs crates\library\src\metadata.rs crates\library\src\scanner.rs
+
+crates\library\src\lib.rs:
+crates\library\src\error.rs:
+crates\library\src\import.rs:
+crates\library\src\manager.rs:
+crates\library\src\metadata.rs:
+crates\library\src\scanner.rs:
