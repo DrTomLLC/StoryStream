0c95bb4f23f711144c434f1b5198dcd337a4b689
DrTomLLC <DrTomLLC@gmail.com>
2025-10-04 15:24:29 -0400

    chore(autosync): 2025-10-04T15:24:28

--- target/debug/deps/storystream_media_formats-b38fe4bc8aaf272c.d
+++ target/debug/deps/storystream_media_formats-b38fe4bc8aaf272c.d
@@ -1,7 +1,8 @@
-C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_media_formats-b38fe4bc8aaf272c.d: crates\media-formats\src\lib.rs crates\media-formats\src\format.rs crates\media-formats\src\mime.rs
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_media_formats-b38fe4bc8aaf272c.d: crates\media-formats\src\lib.rs crates\media-formats\src\detection.rs crates\media-formats\src\format.rs crates\media-formats\src\mime.rs
 
-C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_media_formats-b38fe4bc8aaf272c.rmeta: crates\media-formats\src\lib.rs crates\media-formats\src\format.rs crates\media-formats\src\mime.rs
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_media_formats-b38fe4bc8aaf272c.rmeta: crates\media-formats\src\lib.rs crates\media-formats\src\detection.rs crates\media-formats\src\format.rs crates\media-formats\src\mime.rs
 
 crates\media-formats\src\lib.rs:
+crates\media-formats\src\detection.rs:
 crates\media-formats\src\format.rs:
 crates\media-formats\src\mime.rs:
