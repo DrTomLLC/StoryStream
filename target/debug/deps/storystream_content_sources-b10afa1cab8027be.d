50c8ef8fb5bf73e05e2320a697114fb93b0eaf6b
DrTomLLC <DrTomLLC@gmail.com>
2025-10-04 16:11:18 -0400

    chore(autosync): 2025-10-04T16:11:16

--- target/debug/deps/storystream_content_sources-b10afa1cab8027be.d
+++ target/debug/deps/storystream_content_sources-b10afa1cab8027be.d
@@ -1,7 +1,8 @@
-C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_content_sources-b10afa1cab8027be.d: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\traits.rs
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_content_sources-b10afa1cab8027be.d: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\librivox.rs crates\content-sources\src\traits.rs
 
-C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_content_sources-b10afa1cab8027be.rmeta: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\traits.rs
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_content_sources-b10afa1cab8027be.rmeta: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\librivox.rs crates\content-sources\src\traits.rs
 
 crates\content-sources\src\lib.rs:
 crates\content-sources\src\local.rs:
+crates\content-sources\src\librivox.rs:
 crates\content-sources\src\traits.rs:
--- target/debug/deps/storystream_content_sources-e408c762de25a965.d
+++ target/debug/deps/storystream_content_sources-e408c762de25a965.d
@@ -1,7 +1,8 @@
-C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_content_sources-e408c762de25a965.d: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\traits.rs
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream_content_sources-e408c762de25a965.d: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\librivox.rs crates\content-sources\src\traits.rs
 
-C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_content_sources-e408c762de25a965.rmeta: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\traits.rs
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream_content_sources-e408c762de25a965.rmeta: crates\content-sources\src\lib.rs crates\content-sources\src\local.rs crates\content-sources\src\librivox.rs crates\content-sources\src\traits.rs
 
 crates\content-sources\src\lib.rs:
 crates\content-sources\src\local.rs:
+crates\content-sources\src\librivox.rs:
 crates\content-sources\src\traits.rs:
