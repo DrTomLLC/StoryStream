d2e61f6d75f1c1563933afe94ff7b63f2fa385e4
DrTomLLC <DrTomLLC@gmail.com>
2025-10-04 15:59:22 -0400

    chore(autosync): 2025-10-04T15:59:21

---
+++ target/debug/deps/storystream-6430857c467ce91e.d
@@ -0,0 +1,8 @@
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream-6430857c467ce91e.d: crates\cli\src\main.rs crates\cli\src\commands.rs crates\cli\src\player.rs crates\cli\src\ui.rs
+
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream-6430857c467ce91e.rmeta: crates\cli\src\main.rs crates\cli\src\commands.rs crates\cli\src\player.rs crates\cli\src\ui.rs
+
+crates\cli\src\main.rs:
+crates\cli\src\commands.rs:
+crates\cli\src\player.rs:
+crates\cli\src\ui.rs:
---
+++ target/debug/deps/storystream-a604d0e3e305b4e8.d
@@ -0,0 +1,9 @@
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\storystream-a604d0e3e305b4e8.d: crates\cli\src\main.rs crates\cli\src\commands.rs crates\cli\src\commands\tests.rs crates\cli\src\player.rs crates\cli\src\ui.rs
+
+C:\Users\DrTom\RustroverProjects\StoryStream\target\debug\deps\libstorystream-a604d0e3e305b4e8.rmeta: crates\cli\src\main.rs crates\cli\src\commands.rs crates\cli\src\commands\tests.rs crates\cli\src\player.rs crates\cli\src\ui.rs
+
+crates\cli\src\main.rs:
+crates\cli\src\commands.rs:
+crates\cli\src\commands\tests.rs:
+crates\cli\src\player.rs:
+crates\cli\src\ui.rs:
